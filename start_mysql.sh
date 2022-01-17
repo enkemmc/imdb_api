d489abaa511708709c4bc437f4883c145d64a95b
Mark Enkema <markenkema@gmail.com>
2022-01-16 23:55:10 -0800

    removed -v arg from mysql script that was persisting the db locally

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,6 +1,5 @@
 docker run -dp 3306:33060 \
     --network imdb-scrape --network-alias mysql \
-    -v imdb-mysql-data:/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=secret \
     -e MYSQL_DATABASE=imdb \
     mysql:5.7
